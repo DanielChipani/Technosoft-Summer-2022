b6dc2d31e35b47673f475f038f5ac1920f0ae07a
Daniel Chipani <Chipanidan@gmail.com>
2022-07-02 16:18:27 -0400

    hello World

--- Class1/Homework1.js
+++ Class1/Homework1.js
@@ -18,3 +18,26 @@
  * 
  */
 
+ let fTemp1 = 75;
+ let cTemp1 = (fTemp1 - 32) * 5/9;
+ console.log(`\n${fTemp1}°F is equals to ${cTemp1}°C`);
+
+ let fTemp2 = 90;
+ let kTemp1 = (fTemp2) + 459.67 * 5/9; 
+ console.log(`\n${fTemp1}°F is equal to ${kTemp1}°K`);
+
+ let cTemp2 = 22;
+ let fTemp3 = (cTemp2) * 9/5 + 32;
+ console.log(`\n${cTemp2}°C is equal to ${fTemp3}°F`);
+
+ let cTemp3 = 49;
+ let kTemp2 = (cTemp3) + 273.15;
+ console.log(`\n${cTemp3}°C is equal to ${kTemp2}°K`);
+
+ let kTemp4 = 53;
+ let fTemp4 = (kTemp4) * 9/5 - 459.63;
+ console.log(`\n${kTemp4}°K is equal to ${fTemp4}°F`);
+
+ let kTemp5 = 88;
+ let cTemp4 = (kTemp5) - 273.15;
+ console.log(`\n${kTemp5}°K is equal to ${cTemp4}°C`);
