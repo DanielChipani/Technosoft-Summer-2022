6fe3f63667edd6e969c1965c259fcb5a452df55f
Daniel Chipani <Chipanidan@gmail.com>
2022-07-02 14:38:13 -0400

    Hello World

---
+++ Class1/Homework1.js
@@ -0,0 +1,20 @@
+/**
+ * Convert temperature values into different units
+ * 
+ * F -> C
+ * F -> K
+ * 
+ * C -> F
+ * C -> K
+ * 
+ * K -> F
+ * K -> C
+ * 
+ * Refer link for formula:
+ * https://www.rapidtables.com/convert/temperature/fahrenheit-to-celsius.html
+ * 
+ * 
+ * Due Date: Jul-3 EOD
+ * 
+ */
+
