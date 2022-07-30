50247b879c38af431896eeb2d1a44eb94fc431fa
Daniel Chipani <Chipanidan@gmail.com>
2022-07-29 21:00:07 -0400

    code call

---
+++ Class1/Codetestfun.js
@@ -0,0 +1,14 @@
+const MyCodeTest = require('./Codetest1');
+
+const mct = new MyCodeTest();
+
+
+let output = mct.toGivArray(2, 5, 9);
+console.log(output);
+
+
+let min = mct.firstMissingPositive(2, 3, 1, 56, 23, 11);
+
+
+
+mct.returnPoints(70, 60)
